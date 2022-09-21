9f7f16c661e33edac8b49ecf9fc3a5ee57adb35c
Felipe <felipe.shimada@kepha.com.br>
2022-09-21 00:55:08 -0300

    Projeto completo

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,40 @@
-import React from 'react';
-import logo from './logo.svg';
+import st1 from './image/st1.jpg'
+import st2 from './image/st2.jpg'
+import st3 from './image/st3.jpg'
+import st4 from './image/st4.jpg'
+import st5 from './image/st5.jpg'
+import st6 from './image/st6.jpg'
+import st7 from './image/st7.jpg'
+import st8 from './image/st8.jpg'
+import { useState } from 'react';
 import './App.css';
 
+function randomNumberInRange(min: number, max: number) {
+  return Math.floor(Math.random() * (max - min + 1)) + min;
+}
+
 function App() {
+
+
+  const [state, setState] = useState(
+    {
+      index: -1,
+      imgList: [st1, st2, st3, st4, st5, st6, st7, st8]
+    }
+  );
+
+  const click = () => {
+    if (state.index + 1 === state.imgList.length) {
+      setState({ index: 0, imgList: [st1, st2, st3, st4, st5, st6, st7, st8] })
+    } else {
+      setState({ index: randomNumberInRange(0, 7), imgList: [st1, st2, st3, st4, st5, st6, st7, st8] })
+    }
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <button className='botao' onClick={click}>Clique aqui</button>
+      <img className='imagem' src={state.imgList[state.index]} alt="Imagens do leleo e sua turminha" />
     </div>
   );
 }
